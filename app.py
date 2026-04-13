053c11bde32589d26fc1b54a8de37d2a569f177a
nandarr21 <topmartis859@gmail.com>
2026-04-13 21:56:35 +0700

    Update project for deployment

--- app.py
+++ app.py
@@ -144,5 +144,6 @@ def all_data():
     return jsonify(result)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
